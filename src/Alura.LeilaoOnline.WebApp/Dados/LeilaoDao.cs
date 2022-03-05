78a96c9a0a649f1be9da216f36225b7ed2e77ccb
Leonardo Cascaes <leonardo.cascaes@hotmail.com>
2022-03-05 17:40:51 -0300

    Extraido codigos duplicados na LeilaoController e LeilaoApiController, colocando esses codigos em uma classe separada chamada LeilaoDao. Seguindo o padrão de responsabilidade unica.

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/LeilaoDao.cs
@@ -0,0 +1,51 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public class LeilaoDao
+    {
+        private readonly AppDbContext _context;
+
+        public LeilaoDao()
+        {
+            _context = new AppDbContext();
+        }
+
+        public void Incluir(Leilao leilao)
+        {
+            _context.Leiloes.Add(leilao);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Leilao> BuscarLeiloes()
+        {
+            return _context.Leiloes
+                .Include(l => l.Categoria);
+        }
+
+        public Leilao BuscarPorId(int id)
+        {
+            return _context.Leiloes.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Alterar(Leilao leilao)
+        {
+            _context.Leiloes.Update(leilao);
+            _context.SaveChanges();
+        }
+
+        public void Excluir(Leilao leilao)
+        {
+            _context.Leiloes.Remove(leilao);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Categoria> BuscarCategorias()
+        {
+            return _context.Categorias.ToList();
+        }
+    }
+}
